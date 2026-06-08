ebbfd2a2f7f306ea94e7cba73db4ebff90e050a4
Mubashir Khalil <mubashirkhalilahmad@gmail.com>
2026-06-08 12:01:04 +0500

    update project

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs'
     
 ]
 
@@ -121,3 +122,6 @@ STATICFILES_DIRS =[
     'blog_main/static',
 ]
 #after this load static in home
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
